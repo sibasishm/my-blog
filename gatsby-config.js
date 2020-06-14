0275101744be76a29163b615ae691d4ae8857c22
Sibasish Mohanty <smsibasish@gmail.com>
2020-06-14 19:40:44 +0530

    some final touches

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
 			resolve: 'gatsby-source-filesystem',
 			options: {
 				name: 'markdown-pages',
-				path: `${__dirname}/blog`
+				path: `${__dirname}/content/blogs`
 			}
 		}
 	]
